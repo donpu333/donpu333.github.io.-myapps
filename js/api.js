da83fbcbdfc2964df83d4b964469e0d822ea741e
donpu333 <donpupi61@gmail.com>
2025-06-14 12:21:04 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,22 @@
+class BinanceAPIManager {
+    constructor() {
+        // Инициализация
+    }
 
+    async init() {
+        // Подключение к API
+    }
+
+    async getCurrentPrice(symbol, marketType) {
+        // Получение текущей цены
+    }
+}
+
+// Глобальный экземпляр API
+let apiManager;
+
+// Инициализация при загрузке страницы
+document.addEventListener('DOMContentLoaded', async () => {
+    apiManager = new BinanceAPIManager();
+    await apiManager.init();
+});
