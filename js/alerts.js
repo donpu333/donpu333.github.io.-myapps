c43f8f847781d92064f358bf114e42b37114fcd0
donpu333 <donpupi61@gmail.com>
2025-06-14 12:21:26 +0300

    Update alerts.js

--- js/alerts.js
+++ js/alerts.js
@@ -1,1 +1,16 @@
+let userAlerts = [];
+let currentAlertFilter = 'active';
 
+function loadUserAlerts(filter = 'active') {
+    // Загрузка и отображение алертов
+}
+
+function addUserAlert(symbol, type, condition, value, notificationMethods, notificationCount, chatId) {
+    // Добавление нового алерта
+}
+
+function checkAlerts() {
+    // Проверка срабатывания алертов
+}
+
+// Другие функции для работы с алертами...
