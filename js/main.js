104df4681cde7ec5e397ac659d5ca99dd773a6aa
donpu333 <donpupi61@gmail.com>
2025-06-14 12:20:43 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+// Общие функции для всех страниц
+function showNotification(title, message) {
+    // Реализация уведомлений
+}
 
+function toggleMenu() {
+    // Логика меню
+}
+
+// Другие общие функции...
