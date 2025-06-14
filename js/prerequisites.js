2b14857938202225d08df22ef9cfecee500e88e8
donpu333 <donpupi61@gmail.com>
2025-06-14 12:22:14 +0300

    Update prerequisites.js

--- js/prerequisites.js
+++ js/prerequisites.js
@@ -1,1 +1,11 @@
+let currentVisibleText = null;
 
+function toggleText(type) {
+    // Переключение между чек-листами
+}
+
+function getSelectedItems() {
+    // Получение выбранных пунктов
+}
+
+// Другие функции для работы с предпосылками...
