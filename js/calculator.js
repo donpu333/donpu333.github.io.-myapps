b29567b8e5d8b051f8865ee00952a650c8c4ec32
donpu333 <donpupi61@gmail.com>
2025-06-14 12:21:56 +0300

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,13 @@
+let isLong = true;
+let stopMethod = 'atr';
+let tradeType = 'long-breakout';
 
+function initCalculator() {
+    // Инициализация калькулятора
+}
+
+function calculateRisk() {
+    // Расчет рисков
+}
+
+// Другие функции калькулятора...
